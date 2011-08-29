660a5c3544c791289dae4f7c6e792a4ca7224df9
Stephen Leacock <steveleacock@yahoo.com>
2011-08-29 15:06:12 -0400

    new function has been added myipaddress();

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+  class helloworld{
+     
+     
+      function hellworld($information){
+         echo "Hello world $information";
+      }
+      function myipaddress(){
+       echo $_SERVER['REMOTE_ADDR'];
+      }
+  } 
+    
+$new = new helloworld();
+$new->helloworld('Stephen');
+?>
